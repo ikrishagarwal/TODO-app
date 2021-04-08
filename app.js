d5afaf35305d4d599aa6497a54bc496ef62d8d44
KrishAgarwal2811 <krishagarwal2811@gmail.com>
2021-04-08 13:12:13 +0530

    CHNAGES :- Added css variables for dark and light mode. Made some minor chnages to html. Added javascript to add TODOs Also a setting for vs code tab space

--- app.js
+++ app.js
@@ -2,6 +2,31 @@
 
 const formSubit = (e) => {
   e.preventDefault();
+
+  let input = document.querySelector("#input");
+  let content = input.value;
+
+  input.value = "";
+
+  let todo = document.createElement("div");
+  todo.classList.add("todo");
+
+  let html = `
+    <p class="content">${content}</p>
+    <div class="buttons">
+      <button class="done">
+        <img src="./assets/done.svg" alt="done" />
+      </button>
+      <button class="delete">
+        <img src="./assets/delete.svg" alt="delete" />
+      </button>
+    </div>
+  `;
+
+  todo.innerHTML = html;
+
+  let TODOs = document.querySelector("#TODOs");
+  TODOs.insertAdjacentElement("afterbegin", todo);
 };
 
 const init = () => {
