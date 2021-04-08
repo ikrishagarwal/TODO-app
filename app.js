3b417594d8ddcb64a4b04b9810e78a3403d880e6
KrishAgarwal2811 <krishagarwal2811@gmail.com>
2021-04-08 13:51:01 +0530

    ADDED :- - Done functionaity - Delete functionality - Some CSS animations for these functions.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 "use strict";
 
+// FORM SUBMITED AND NOW ADD IT AS A NEW TODO
 const formSubit = (e) => {
   e.preventDefault();
 
@@ -14,10 +15,10 @@ const formSubit = (e) => {
   let html = `
     <p class="content">${content}</p>
     <div class="buttons">
-      <button class="done">
+      <button onclick="done(event)" class="done">
         <img src="./assets/done.svg" alt="done" />
       </button>
-      <button class="delete">
+      <button onclick="deleteTodo(event)" class="delete">
         <img src="./assets/delete.svg" alt="delete" />
       </button>
     </div>
@@ -29,9 +30,39 @@ const formSubit = (e) => {
   TODOs.insertAdjacentElement("afterbegin", todo);
 };
 
+// TODO COMPLETED
+const done = (e) => {
+  const todo = e.target.parentElement.parentElement.parentElement;
+  todo.classList.toggle("todo-done");
+};
+
+// DELETE TODO
+const deleteTodo = (e) => {
+  const todo = e.target.parentElement.parentElement.parentElement;
+  todo.classList.add("todo-delete");
+
+  window.setTimeout(() => {
+    const TODOs = document.querySelector("#TODOs");
+    TODOs.removeChild(todo);
+  }, 1500);
+};
+
 const init = () => {
+  // FORM FOR ADDING TODOs
   const formHolder = document.querySelector("#input-container");
   formHolder.addEventListener("submit", formSubit);
+
+  // ADDING EVENT TO DONE BUTTON
+  const doneBtn = document.querySelectorAll(".done");
+  doneBtn.forEach((btn) => {
+    btn.addEventListener("click", done);
+  });
+
+  // ADDING EVENT TO DELETE BUTTON
+  const deleteBtn = document.querySelectorAll(".delete");
+  deleteBtn.forEach((btn) => {
+    btn.addEventListener("click", deleteTodo);
+  });
 };
 
 window.addEventListener("DOMContentLoaded", init);
